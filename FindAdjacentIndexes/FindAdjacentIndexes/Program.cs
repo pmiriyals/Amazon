5e08d363a111880502ef31abb90c1d546e6a8a29
Praneeth Miriyala <pmiriyal@gmail.com>
2013-08-23 08:42:56 -0400

    new programs

---
+++ FindAdjacentIndexes/FindAdjacentIndexes/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FindAdjacentIndexes
+{
+    class Program
+    {
+        //Construct MinHeap of Array Objects, where Objects contains (val, index), Sort by val and get the indexes: O(n log n) time and O(n) space, will be problematic with equal elements
+        //Use a Dictionary<val, List<index>>: Sorting Dictionary by val is an issue
+
+        class node
+        {
+            public int val { get; set; }
+            public int index { get; set; }
+            public node(int val, int index)
+            {
+                this.val = val;
+                this.index = index;
+            }
+        }
+
+        static void heapify(node[] arr)
+        {
+            int end = arr.Length - 1;
+            int start = (end - 1) / 2;
+
+            while (start >= 0)
+            {
+                siftDown(arr, start, end);
+                start--;
+            }
+        }
+
+        static void siftDown(node[] arr, int start, int end)
+        {
+            int root = start;
+
+            while ((2 * root + 1) <= end)
+            {
+                int swap = root;
+                int child = 2 * root + 1;
+
+                if (arr[swap].val < arr[child].val)
+                    swap = child;
+
+                if ((child + 1) <= end && arr[swap].val < arr[child + 1].val)
+                    swap = child + 1;
+
+                if (root != swap)
+                {
+                    node temp = arr[swap];
+                    arr[swap] = arr[root];
+                    arr[root] = temp;
+                    root = swap;
+                }
+                else
+                    return;
+            }
+        }
+
+        static void heapSort(node[] arr)
+        {
+            heapify(arr);;
+            int end = arr.Length - 1;
+            while (end > 0)
+            {
+                node temp = arr[0];
+                arr[0] = arr[end];
+                arr[end] = temp;
+                end--;
+                siftDown(arr, 0, end);
+            }
+        }
+        
+        static void FindAdjacentIndex(int[] arr)
+        {
+            node[] n = new node[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                n[i] = new node(arr[i], i);
+            }
+
+            heapSort(n);
+            Console.WriteLine("After sorting elements: ");
+            foreach (node ele in n)
+            {
+                Console.WriteLine("val = {0} & index = {1}", ele.val, ele.index);
+            }
+
+            Console.WriteLine("Adjacent Pairs: ");
+
+            Dictionary<int, List<int>> dict = new Dictionary<int, List<int>>();
+
+            //node cur = n[0];
+            //int prevIndex = cur.index;
+            //List<int> dup = new List<int>();
+            //for(int i = 1; i < n.Length; i++)
+            //{
+            //    if (cur.val == n[i].val)
+            //    {
+            //        dup.Add(n[i].val);
+            //    }
+            //    else if (dup.Count > 0)
+            //    {
+            //        foreach (int j in dup)
+            //        {
+            //            Console.Write("({0}, {1}) ", prevIndex, j);
+            //            Console.Write("({0}, {1}) ", j, n[i].index);
+            //        }
+            //        dup.Clear();
+            //        Console.WriteLine();
+
+            //        Console.WriteLine("({0}, {1})", cur.index, n[i].index);
+            //        prevIndex = cur.index;
+            //        cur = n[i];
+            //    }
+            //    else
+            //    {                    
+            //        Console.WriteLine("({0}, {1})", cur.index, n[i].index);
+            //        prevIndex = cur.index;
+            //        cur = n[i];
+            //    }
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = { 0, 3, 3, 7, 5, 3, 11, 1};
+            FindAdjacentIndex(arr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MedianOfStreamOfIntegers/MedianOfStreamOfIntegers/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MedianOfStreamOfIntegers
+{
+    class Program
+    {
+        //median is the root element
+        //minHeap contains set of all elemenst greater than the one's in maxHeap, that is minHeap is the right sub arr of a sorted list
+        //similarly maxHeap is the leftSub arr of a sorted list
+        
+        static List<int> minHeap = new List<int>();
+        static List<int> maxHeap = new List<int>();
+        static int median = -1;
+
+
+        static int Median(int data)
+        {
+            if (median < 0) //base case for the 1st element (entire heap is empty at the moment)
+            {
+                median = data;
+                minHeap.Add(data); // add to the right sub tree 
+            }
+            else
+            {
+                bool siftMin = false;
+                bool siftMax = false;
+                
+                if (data < minHeap[0])  //then element should go into left sub tree
+                {
+                    if (maxHeap.Count <= minHeap.Count)
+                    {
+                        maxHeap.Add(data);
+                        siftMax = true;
+                    }
+                    else if (data < maxHeap[0])  //copy root of maxHeap to minHeap and add data to maxHeap, so that difference always remains at most 1
+                    {
+                        minHeap.Add(maxHeap[0]);
+                        maxHeap[0] = data;  //data is max in maxheap
+                        siftMin = true;
+                    }
+                    else
+                    {
+                        minHeap.Add(data);
+                        siftMin = true;
+                    }
+                }
+                else
+                {
+                    if (minHeap.Count <= maxHeap.Count)
+                    {
+                        minHeap.Add(data);
+                        siftMin = true;
+                    }
+                    else
+                    {
+                        maxHeap.Add(minHeap[0]);    //if data is greater than min of right subarr, then move min to left subarr and copy data to right subarr (when minHeap count is more)
+                        minHeap[0] = data;
+                        siftMax = true;
+                    }
+                }
+
+                if(siftMin)
+                    minSiftDown(minHeap, 0, minHeap.Count - 1);
+                if(siftMax)
+                    maxSiftDown(maxHeap, 0, maxHeap.Count - 1);
+
+                if (minHeap.Count == maxHeap.Count)
+                    median = (minHeap[0] + maxHeap[0]) / 2;
+                else if (minHeap.Count > maxHeap.Count)
+                    median = minHeap[0];
+                else
+                    median = maxHeap[0];
+            }
+
+            return median;
+        }
+
+        //static void minHeapify(List<int> arr)
+        //{
+        //    int end = arr.Count - 1;
+        //    int start = (end - 1) / 2;
+
+        //    while (start >= 0)
+        //    {
+                
+        //    }
+        //}
+
+        static void maxSiftDown(List<int> data, int start, int end)
+        {
+            int root = start;
+
+            while ((root * 2 + 1) <= end)
+            {
+                int child = root * 2 + 1;
+                int swap = root;
+
+                if (data[swap] < data[child])
+                    swap = child;
+
+                if ((child + 1) <= end && data[swap] < data[child + 1])
+                    swap = child + 1;
+
+                if (root != swap)
+                {
+                    int temp = data[root];
+                    data[root] = data[swap];
+                    data[swap] = temp;
+
+                    root = swap;
+                }
+                else
+                    return;
+            }
+        }
+
+        static void minSiftDown(List<int> data, int start, int end)
+        {
+            int root = start;
+
+            while ((root * 2 + 1) <= end)
+            {
+                int child = root * 2 + 1;
+                int swap = root;
+
+                if (data[swap] > data[child])
+                    swap = child;
+
+                if ((child + 1) <= end && data[swap] > data[child + 1])
+                    swap = child + 1;
+
+                if (root != swap)
+                {
+                    int temp = data[root];
+                    data[root] = data[swap];
+                    data[swap] = temp;
+
+                    root = swap;
+                }
+                else
+                    return;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 15, 1, 3, 2, 8, 7, 9, 10, 6, 11, 4 };
+
+            foreach (int i in arr)
+                Console.WriteLine("Median = {0} (after inserting {1})", Median(i), i);
+            Console.ReadLine();
+        }
+    }
+}
